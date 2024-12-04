734f08ea4cbcbd2e2b06f37d2556b54494241588
bhyzied <zied.bhy89@gmail.com>
2024-12-04 16:49:49 +0100

    source spring intialzr

---
+++ src/test/java/com/zied/hibernate/hibernate/HibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zied.hibernate.hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
